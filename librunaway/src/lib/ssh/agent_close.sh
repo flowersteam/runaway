7646ae43183d5242917af30487437d59ae5c89a8
Alexandre Péré <alexandre.pere@protonmail.com>
2020-02-19 16:06:58 +0100

    Changes according to shellcheck

--- librunaway/src/lib/ssh/agent_close.sh
+++ librunaway/src/lib/ssh/agent_close.sh
@@ -2,14 +2,14 @@
 rw_close(){
 
     # We output the current working directory
-    printf 'RUNAWAY_CWD: %s\n' $(pwd)
+    printf 'RUNAWAY_CWD: %s\n' "$(pwd)"
 
     # We echo the environment variables
     env | sed 's/^/RUNAWAY_ENV: /g'
 
     # We cleanup
-    rm $stdout_fifo
-    rm $stderr_fifo
+    rm "$stdout_fifo"
+    rm "$stderr_fifo"
 
     # We leave
     echo 'RUNAWAY_EOF:'
--- librunaway/src/lib/ssh/agent_init.sh
+++ librunaway/src/lib/ssh/agent_init.sh
@@ -1,7 +1,9 @@
 rw_init() {
     # We generate two fifo that will be used to format stdout messages and stderr messages
-    export stdout_fifo=/tmp/$(uuidgen)
-    export stderr_fifo=/tmp/$(uuidgen)
-    mkfifo $stdout_fifo
-    mkfifo $stderr_fifo
+    stdout_fifo="/tmp/$(uuidgen)"
+    export stdout_fifo
+    stderr_fifo="/tmp/$(uuidgen)"
+    export stderr_fifo
+    mkfifo "$stdout_fifo"
+    mkfifo "$stderr_fifo"
 }
